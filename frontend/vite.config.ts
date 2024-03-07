8e4ca8f6b26df499e7ea93259648b60ac15b3207
정은수 <qufvkdlej@naver.com>
2024-03-07 20:23:24 +0900

    init: frontend proejct

---
+++ frontend/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+            },
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true,
+                globPatterns: ['**/*.{js,ts,css,html,ico,png,svg}'],
+            },
+            manifest: {
+                name: '떡상',
+                short_name: '떡상',
+                description: '최고의 상인이 되어보세요!',
+                theme_color: '#ffffff',
+                start_url: '/',
+                scope: '.',
+                display: 'fullscreen',
+                orientation: 'landscape',
+                lang: 'ko-KR',
+                icons: [
+                    {
+                        src: '/icons/icon-192.png',
+                        type: 'image/png',
+                        sizes: '192x192',
+                    },
+                    {
+                        src: '/icons/icon-512.png',
+                        type: 'image/png',
+                        sizes: '512x512',
+                    },
+                ],
+            },
+        }),
+    ],
+});
