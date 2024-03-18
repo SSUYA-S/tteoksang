1d29c3bc0dfb1e9c54f27585dcf72c8009e20b92
김강민 <mmm3657@naver.com>
2024-03-18 17:58:50 +0900

    feat: 유저정보 조회 기능 구현

---
+++ backend/src/main/java/com/welcome/tteoksang/user/dto/Title.java
@@ -0,0 +1,24 @@
+package com.welcome.tteoksang.user.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="title")
+public class Title {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "title_id")
+    private int titleId;
+
+    @Column(name = "title_name", nullable = false)
+    private String titleName;
+
+    @Column(name = "title_content")
+    private String titleContent;
+}
--- backend/src/main/java/com/welcome/tteoksang/user/dto/User.java
+++ backend/src/main/java/com/welcome/tteoksang/user/dto/User.java
@@ -3,10 +3,12 @@ package com.welcome.tteoksang.user.dto;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,76 +25,84 @@ import org.springframework.security.core.userdetails.UserDetails;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class User implements UserDetails {
 
-  @Id
-  @Column(name = "user_id")
-  @Builder.Default
-  private String userId = UUID.randomUUID().toString();
+    @Id
+    @Column(name = "user_id")
+    @Builder.Default
+    private String userId = UUID.randomUUID().toString();
 
-  @Column(name="user_google_id", unique = true, nullable = false)
-  private String userGoogleId;
+    @Column(name = "user_google_id", unique = true, nullable = false)
+    private String userGoogleId;
 
-  @Email
-  @Column(name = "user_email", unique = true, nullable = false)
-  private String userEmail;
+    @Email
+    @Column(name = "user_email", unique = true, nullable = false)
+    private String userEmail;
 
-  @NotBlank
-  @Column(name = "user_nickname", unique = true, nullable = false)
-  private String userNickname;
+    @NotBlank
+    @Column(name = "user_nickname", unique = true, nullable = false)
+    private String userNickname;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_profile_icon_id")
-  private ProfileIcon profileIcon;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_profile_icon_id")
+    private ProfileIcon profileIcon;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_profile_frame_id")
-  private ProfileFrame profileFrame;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_profile_frame_id")
+    private ProfileFrame profileFrame;
 
-  @CreationTimestamp(source = SourceType.DB)
-  @Column(name = "created_at", nullable = false)
-  private LocalDateTime createdAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_theme_id")
+    private Theme theme;
 
-  @Column(name = "deleted_at")
-  private LocalDateTime deletedAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_title_id")
+    private Title title;
 
-  @Column(name = "career")
-  private int career;
+    @CreationTimestamp(source = SourceType.DB)
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return new ArrayList<>();
-  }
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
-  @Override
-  public String getPassword() {
-    return "";
-  }
+    @Column(name = "career")
+    private int career;
 
-  @Override
-  public String getUsername() {
-    return userEmail;
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return deletedAt == null;
-  }
+    @Override
+    public String getPassword() {
+        return "";
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return deletedAt == null;
-  }
+    @Override
+    public String getUsername() {
+        return userEmail;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return deletedAt == null;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return deletedAt == null;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
