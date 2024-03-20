ad89236b39ee233ef9e03a276b0ea20957377fef
이선재 <sj06152@naver.com>
2024-03-20 15:48:19 +0900

    [chore] DB에 더미데이터 추가

--- docker-compose/initdb.d/03achievement.sql
+++ docker-compose/initdb.d/03achievement.sql
@@ -11,4 +11,10 @@ create table if not exists achievement
 
 insert into achievement(achievement_name, achievement_content, statistics_name, achievement_goal,
                         achievement_reward_type, achievement_reward_id)
-VALUES ("초보 도매상: 감자를 사요", "감자를 좋아하시나봐요! 벌써 50개를 샀어요", "AccTotalPotatoCount", 50, "NONE", 1);
+VALUES ("초보 도매상: 감자를 사요", "감자를 좋아하시나봐요! 벌써 50개를 샀어요", "AccTotalPotatoCount", 50, "NONE", 0),
+       ("강원도의 전설","강원도의 전설이 되어보세요.","AccProductPurchaseQunatity,1",30,'NONE',0),
+       ("아침드라마","답답하다.","AccProductPurchaseQunatity,2",30,'NONE',0),
+       ("당근마켓 온도 100도","당근마켓에 당근 팔아요^^","AccProductPurchaseQunatity,3",10,"FRAME",2),
+       ("무트코인은 떡상한다.","대량 매수가 답이다","AccProductPurchaseQunatity,4",100,"TITLE",5),
+       ("토마토","거꾸로 말해도 토마토","AccProductPurchaseQunatity,5",100,"TITLE",3)
+;
