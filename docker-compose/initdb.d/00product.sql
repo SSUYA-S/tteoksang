d163f3474f37ff23cf86551e689771c72a5db9dd
이선재 <sj06152@naver.com>
2024-03-20 11:09:21 +0900

    [fix] DB수정: product테이블이 product_fluctuation 테이블로 분리

--- docker-compose/initdb.d/00product.sql
+++ docker-compose/initdb.d/00product.sql
@@ -4,10 +4,9 @@ create table if not exists product(
     product_basic_cost int not null,
     product_type char(10) not null,
     product_unit varchar(10) not null,
-    product_min_rate  int not null,
-    product_max_rate  int not null
+    product_basic_harvest int
 ) ENGINE innoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-insert into product(product_name, product_basic_cost, product_type, product_unit, product_min_rate, product_max_rate)
-values ("감자",1200,"COMMON","개",2,5),
-       ("고구마",1200,"COMMON","개",2,5);
+insert into product(product_name, product_basic_cost, product_type, product_unit, product_basic_harvest)
+values ("감자",1200,"ALL","개",50),
+       ("고구마",1200,"ALL","개",40);
