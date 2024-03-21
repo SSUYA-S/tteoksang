146e751e8d1d504e22e229ca09a963a2eebfe842
김강민 <mmm3657@naver.com>
2024-03-21 13:59:58 +0900

    refactor: 웹소켓 초기 설정 변경

--- backend/src/main/java/com/welcome/tteoksang/game/controller/GameController.java
+++ backend/src/main/java/com/welcome/tteoksang/game/controller/GameController.java
@@ -1,11 +1,13 @@
 package com.welcome.tteoksang.game.controller;
 
 import com.welcome.tteoksang.game.dto.GameMessage;
+import com.welcome.tteoksang.user.dto.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -20,6 +22,7 @@ public class GameController {
              @Payload: 메시지 본문(body)의 내용을 메서드의 인자로 전달할 때 사용
                       (클라이언트가 JSON 형태의 메시지를 보냈다면, 이를 GameMessage 객체로 변환하여 메서드에 전달)
           */
+        User user = (User) ((Authentication) principal).getPrincipal();
         switch (gameMessage.getType()){
 
         }
@@ -34,7 +37,7 @@ public class GameController {
              @Payload: 메시지 본문(body)의 내용을 메서드의 인자로 전달할 때 사용
                       (클라이언트가 JSON 형태의 메시지를 보냈다면, 이를 GameMessage 객체로 변환하여 메서드에 전달)
           */
-
+        User user = (User) ((Authentication) principal).getPrincipal();
         return gameMessage;
     }
 }
