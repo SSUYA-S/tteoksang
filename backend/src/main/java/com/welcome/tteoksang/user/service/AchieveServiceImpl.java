e4025ef290c9b9e4ca42ac4d7901855c0b065d49
김강민 <mmm3657@naver.com>
2024-03-21 12:52:31 +0900

    refactor: 쿠키 지우기 로직 수정

--- backend/src/main/java/com/welcome/tteoksang/user/service/AchieveServiceImpl.java
+++ backend/src/main/java/com/welcome/tteoksang/user/service/AchieveServiceImpl.java
@@ -2,7 +2,7 @@ package com.welcome.tteoksang.user.service;
 
 import com.welcome.tteoksang.user.dto.Achieve;
 
-import com.welcome.tteoksang.user.dto.AchieveRes;
+import com.welcome.tteoksang.user.dto.AchieveInfo;
 import com.welcome.tteoksang.user.repository.AchieveRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +19,11 @@ public class AchieveServiceImpl implements AchieveService {
     private final AchieveRepository achieveRepository;
 
     @Override
-    public List<AchieveRes> searchAllAchieve(String userId) {
+    public List<AchieveInfo> searchAllAchieve(String userId) {
         List<Achieve> achieveList = achieveRepository.findByUserId(userId);
-        List<AchieveRes> achieveResList = new ArrayList<>();
+        List<AchieveInfo> achieveResList = new ArrayList<>();
         for(Achieve achieve : achieveList) {
-            achieveResList.add(new AchieveRes(achieve.getAchievementId(), (achieve.getAchievedDate()).toString()));
+            achieveResList.add(new AchieveInfo(achieve.getAchievementId(), (achieve.getAchievedDate()).toString()));
         }
         return achieveResList;
     }
