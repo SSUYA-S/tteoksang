976a33d8cc2ecb22aca8af18390c7f157563f7a7
김연수 <treamor@naver.com>
2024-03-21 12:55:20 +0900

    build: dev 배포 파일 작성

--- backend/src/main/java/com/welcome/tteoksang/config/RedisConfiguration.java
+++ backend/src/main/java/com/welcome/tteoksang/config/RedisConfiguration.java
@@ -12,9 +12,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ListOperations;
@@ -34,7 +36,8 @@ public class RedisConfiguration {
   private final RedisProperties redisProperties;
 
   @Bean
-  public LettuceConnectionFactory redisConnectionFactory() {
+  @Profile("default")
+  public LettuceConnectionFactory redisDefaultConnectionFactory() {
     String master = redisProperties.getSentinel().getMaster();
     Set<String> nodes = new HashSet<>(redisProperties.getSentinel().getNodes());
     RedisSentinelConfiguration sentinelConfiguration = new RedisSentinelConfiguration(master, nodes);
@@ -48,11 +51,22 @@ public class RedisConfiguration {
   }
 
   @Bean
-  public RedisTemplate<String, Object> objectRedisTemplate() {
+  @Profile("dev")
+  public LettuceConnectionFactory redisDevConnectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+    redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+    redisStandaloneConfiguration.setPort(redisProperties.getPort());
+
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> objectRedisTemplate(LettuceConnectionFactory redisConnectionFactory) {
     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
 
     return redisTemplate;
   }
