9698994a4831b8381c894a90a9a8dfa42a5b52c4
이선재 <sj06152@naver.com>
2024-03-22 17:40:38 +0900

    feat: public 관련 메세지 기능 준비

--- backend/src/main/java/com/welcome/tteoksang/TteoksangApplication.java
+++ backend/src/main/java/com/welcome/tteoksang/TteoksangApplication.java
@@ -2,7 +2,9 @@ package com.welcome.tteoksang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TteoksangApplication {
 
