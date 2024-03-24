290cd7f57f5d3cc0e902527b6f448058f4e5b35d
김연수 <treamor@naver.com>
2024-03-25 08:51:26 +0900

    feat: theme 데이터 생성

--- docker-compose/initdb.d/02theme.sql
+++ docker-compose/initdb.d/02theme.sql
@@ -4,8 +4,15 @@ create table if not exists theme
     theme_name varchar(90) not null
 );
 
-insert into theme(theme_name)
-values ("화창한 집"),
-       ("바다가 보이는 집"),
-       ("기본 테마");
+insert into theme(theme_id, theme_name)
+values (1, '화창한 집'),
+       (2, '바다가 보이는 집'),
+       (3, '회의실 앞 바닷가'),
+       (4, '마법사의 집'),
+       (5, '시골 문구점'),
+       (6, '아침의 축제'),
+       (7, '덥디 더운 여름'),
+       (8, '한적한 테라스'),
+       (9, '어느 화가의 공방'),
+       (10, '떡상의 문단속');
 
