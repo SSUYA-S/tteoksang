4e9cbf0da6eff52f7956ac7fa2f2b7441591ca67
김연수 <treamor@naver.com>
2024-03-25 00:17:16 +0900

    feat: product, product_fluctuation 데이터 생성

--- docker-compose/initdb.d/00product_fluctuation.sql
+++ docker-compose/initdb.d/00product_fluctuation.sql
@@ -1,9 +1,15 @@
 create table if not exists product_fluctuation
 (
-    product_id         int,
-    count_per_ten_days int,
-    product_min_rate   int not null,
-    product_max_rate   int not null,
-    primary key(product_id,count_per_ten_days),
+    count_per_ten_days int not null,
+    product_id         int not null,
+    product_min_rate   double not null,
+    product_max_rate   double not null,
+    primary key(count_per_ten_days, product_id),
     foreign key(product_id) references product(product_id) on delete cascade
 );
+
+load data infile '/docker-entrypoint-initdb.d/product_fluctuation.csv'
+into table product_fluctuation
+fields terminated by ','
+lines terminated by '\n'
+ignore 1 rows;
