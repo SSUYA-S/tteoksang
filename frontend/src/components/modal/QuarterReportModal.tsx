353d8ef323b417bca1bea3f8817c51ad181633d6
김태운 <twnkm7089@gmail.com>
2024-03-26 09:55:43 +0900

    proceed: 분기 모달 제작중

--- frontend/src/components/modal/QuarterReportModal.tsx
+++ frontend/src/components/modal/QuarterReportModal.tsx
@@ -7,23 +7,41 @@ interface Prop {
     titleList: Title[];
     eventList: Event[];
     productList: Product[];
+    setIsQtrReportAvail: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function QuarterReportModal(props: Prop) {
-    const [isRentModalAvail, setIsRentModalAvail] = useState<boolean>(true);
+    const [mode, setMode] = useState<number>(0);
+
+    const showReport = () => {
+        console.log('hello');
+        setMode(1);
+    };
 
     return (
         <>
-            {isRentModalAvail ? (
+            {mode === 0 ? (
                 <RentFeeModal
                     rentFeeInfo={Quarter.rentFeeInfo}
                     startTurn={Quarter.turn - 91}
                     endTurn={Quarter.turn - 1}
-                    setIsRentModalAvail={setIsRentModalAvail}
+                    showReport={showReport}
                     productList={props.productList}
                 />
             ) : (
-                <></>
+                <>
+                    <div className="absolute w-full h-full top-0 left-0 bg-black opacity-50 z-10"></div>
+                    <div className="w-[60%] h-[60%] absolute left-[20%] top-[20%] color-border-subbold border-[0.5vw] bg-white z-20">
+                        <div
+                            className="absolute text-[2vw] flex items-center justify-center text-white -top-[1.6vw] -right-[2vw] w-[4vw] h-[4vw] border-[0.4vw] color-border-sublight color-bg-orange1 rounded-full cursor-pointer z-30"
+                            onClick={() => {
+                                props.setIsQtrReportAvail(false);
+                            }}
+                        >
+                            X
+                        </div>
+                    </div>
+                </>
             )}
         </>
     );
