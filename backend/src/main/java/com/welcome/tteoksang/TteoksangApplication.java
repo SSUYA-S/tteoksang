6fe424b373408ba4ec2e7bf850707df7cd5fed47
이선재 <sj06152@naver.com>
2024-03-27 12:43:10 +0900

    proceeding: mongoDB 연결 보류

--- backend/src/main/java/com/welcome/tteoksang/TteoksangApplication.java
+++ backend/src/main/java/com/welcome/tteoksang/TteoksangApplication.java
@@ -2,9 +2,15 @@ package com.welcome.tteoksang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
+@EnableJpaRepositories
 @EnableMongoRepositories
 @EnableScheduling
 @SpringBootApplication
