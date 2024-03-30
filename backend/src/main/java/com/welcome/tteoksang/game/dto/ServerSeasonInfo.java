d4b19cc0c7b46557c9e8a900eb94ad3eb3a9ec61
이선재 <sj06152@naver.com>
2024-03-30 22:32:18 +0900

    feat: 뉴스 선정 및 발생 이벤트 결정 로직 구현

---
+++ backend/src/main/java/com/welcome/tteoksang/game/dto/ServerSeasonInfo.java
@@ -0,0 +1,20 @@
+package com.welcome.tteoksang.game.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "_server_season_info")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ServerSeasonInfo {
+    @Id
+    Integer seasonId;
+
+    LocalDateTime startedAt;
+}
--- backend/src/main/java/com/welcome/tteoksang/resource/repository/EventRepository.java
+++ backend/src/main/java/com/welcome/tteoksang/resource/repository/EventRepository.java
@@ -2,11 +2,11 @@ package com.welcome.tteoksang.resource.repository;
 
 import com.welcome.tteoksang.resource.dto.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public interface EventRepository extends MongoRepository<Event,String> {
-
+public interface EventRepository extends MongoRepository<Event, String> {
+    @Query("{$or :[{eventType: ?0},{eventType: SPECIAL}]}")
+    List<Event> findEventsOccurInSpecificSeason(String seasonName);
 }
