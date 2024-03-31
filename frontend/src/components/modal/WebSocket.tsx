a31c693f7d89129fde97d3352108042cc498e1de
김태운 <twnkm7089@gmail.com>
2024-03-31 16:54:59 +0900

    feat: 최대, 최소 경고 추가

--- frontend/src/components/modal/WebSocket.tsx
+++ frontend/src/components/modal/WebSocket.tsx
@@ -76,10 +76,14 @@ export default function WebSocket(props: Prop) {
                                                 info.productInfoList
                                             )
                                         );
+                                        // dispatch(
+                                        //     buyableProductIdState(
+                                        //         info.buyAbleProductIdList
+                                        //     )
+                                        // );
+                                        //더미데이터 수정할 것!!!
                                         dispatch(
-                                            buyableProductIdState(
-                                                info.buyAbleProductIdList
-                                            )
+                                            buyableProductIdState([1, 2, 5, 6])
                                         );
                                         dispatch(
                                             specialEventState(
