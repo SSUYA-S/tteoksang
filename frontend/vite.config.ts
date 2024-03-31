8d90ac0869a0147821c16273dc94e180a323dee4
김연수 <treamor@naver.com>
2024-03-31 22:44:17 +0900

    build: disable PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,37 +5,37 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            workbox: {
-                clientsClaim: true,
-                skipWaiting: true,
-                globPatterns: ['**/*.{js,tsx,ts,tsx,css,html,ico,png,svg}'],
-            },
-            manifest: {
-                name: '떡상',
-                short_name: '떡상',
-                description: '최고의 상인이 되어보세요!',
-                theme_color: '#ffffff',
-                start_url: '/',
-                display: 'fullscreen',
-                orientation: 'landscape',
-                lang: 'ko-KR',
-                icons: [
-                    {
-                        src: '/icons/icon-192.png',
-                        type: 'image/png',
-                        sizes: '192x192',
-                    },
-                    {
-                        src: '/icons/icon-512.png',
-                        type: 'image/png',
-                        sizes: '512x512',
-                    },
-                ],
-            },
-        }),
+        react()
+        // VitePWA({
+        //     registerType: 'autoUpdate',
+        //     workbox: {
+        //         clientsClaim: true,
+        //         skipWaiting: true,
+        //         globPatterns: ['**/*.{js,tsx,ts,tsx,css,html,ico,png,svg}'],
+        //     },
+        //     manifest: {
+        //         name: '떡상',
+        //         short_name: '떡상',
+        //         description: '최고의 상인이 되어보세요!',
+        //         theme_color: '#ffffff',
+        //         start_url: '/',
+        //         display: 'fullscreen',
+        //         orientation: 'landscape',
+        //         lang: 'ko-KR',
+        //         icons: [
+        //             {
+        //                 src: '/icons/icon-192.png',
+        //                 type: 'image/png',
+        //                 sizes: '192x192',
+        //             },
+        //             {
+        //                 src: '/icons/icon-512.png',
+        //                 type: 'image/png',
+        //                 sizes: '512x512',
+        //             },
+        //         ],
+        //     },
+        // }),
     ],
     //proxy settings
     server: {
