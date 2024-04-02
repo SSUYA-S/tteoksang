ae6acb5c7d394124c8093dbc3f122d5ca4aae2cc
김태운 <twnkm7089@gmail.com>
2024-04-02 17:57:46 +0900

    fix: 새로 시작하기 부분 개선

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -242,6 +242,7 @@ export default function MainPage() {
             {!startFlag ? (
                 <GameStartComponent
                     setStartFlag={setStartFlag}
+                    startFlag={startFlag}
                     achievementData={initialData.achievementList}
                     titleData={initialData.titleList}
                     profileFrameData={initialData.profileFrameList}
