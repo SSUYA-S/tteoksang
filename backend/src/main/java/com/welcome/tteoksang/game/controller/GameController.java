d0baf2d80d9494928c3a357ba43fc5b50885f5b0
김강민 <mmm3657@naver.com>
2024-04-02 09:24:33 +0900

    refactor: 결산 더미 데이터 생성

--- backend/src/main/java/com/welcome/tteoksang/game/controller/GameController.java
+++ backend/src/main/java/com/welcome/tteoksang/game/controller/GameController.java
@@ -8,6 +8,7 @@ import com.welcome.tteoksang.game.dto.res.WebSocketIdRes;
 import com.welcome.tteoksang.game.dto.result.TestExample;
 import com.welcome.tteoksang.game.dto.result.end.FinalResult;
 import com.welcome.tteoksang.game.dto.result.half.Half;
+import com.welcome.tteoksang.game.dto.result.offline.OfflineReport;
 import com.welcome.tteoksang.game.dto.result.quarter.Quarter;
 import com.welcome.tteoksang.game.service.ChatService;
 import com.welcome.tteoksang.redis.RedisPrefix;
@@ -83,321 +84,68 @@ public class GameController {
 
     @GetMapping("/half/{webSocketId}")
     public void sendHalf(@PathVariable("webSocketId") String webSocketId) {
-        String logData = TestExample.half;
+        String responseData = TestExample.half;
         ObjectMapper mapper = new ObjectMapper();
         boolean isSuccess = false;
         Half message = null;
         try {
-            message = mapper.readValue(logData, Half.class);
+            message = mapper.readValue(responseData, Half.class);
             isSuccess = true;
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        GameMessageRes quarterResult = GameMessageRes.builder()
+        GameMessageRes halfResult = GameMessageRes.builder()
                 .type(MessageType.HALF_REPORT)
                 .isSuccess(isSuccess)
                 .body(message)
                 .build();
 
-        simpMessagingTemplate.convertAndSend("/topic/private/" + webSocketId, quarterResult);
+        simpMessagingTemplate.convertAndSend("/topic/private/" + webSocketId, halfResult);
     }
 
     @GetMapping("/final/{webSocketId}")
     public void sendFinal(@PathVariable("webSocketId") String webSocketId) {
-        String logData = TestExample.finalReport;
+        String responseData = TestExample.finalReport;
         ObjectMapper mapper = new ObjectMapper();
         boolean isSuccess = false;
         FinalResult message = null;
         try {
-            message = mapper.readValue(logData, FinalResult.class);
+            message = mapper.readValue(responseData, FinalResult.class);
             isSuccess = true;
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        GameMessageRes quarterResult = GameMessageRes.builder()
+        GameMessageRes finalResult = GameMessageRes.builder()
                 .type(MessageType.FINAL_REPORT)
                 .isSuccess(isSuccess)
                 .body(message)
                 .build();
 
-        simpMessagingTemplate.convertAndSend("/topic/private/" + webSocketId, quarterResult);
+        simpMessagingTemplate.convertAndSend("/topic/private/" + webSocketId, finalResult);
     }
 
     @GetMapping("/offline/{webSocketId}")
     public void sendOffline(@PathVariable("webSocketId") String webSocketId) {
-        String logData = "{\n" +
-                "    \"lastGameTurn\": 78,\n" +
-                "    \"rentFeeInfo\": {\n" +
-                "        \"billType\": \"overdue\",\n" +
-                "        \"rentFee\": 10000,\n" +
-                "        \"productList\": [\n" +
-                "            {\n" +
-                "                \"productId\": 2,\n" +
-                "                \"productQuantity\": 20\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"productId\": 3,\n" +
-                "                \"productQuantity\": 5\n" +
-                "            }\n" +
-                "        ]\n" +
-                "    },\n" +
-                "    \"halfReport\": {\n" +
-                "        \"totalProductIncome\": 5000000,\n" +
-                "        \"totalProductOutcome\": 2500000,\n" +
-                "        \"totalBrokerFee\": 120000,\n" +
-                "        \"totalUpgradeFee\": 50000,\n" +
-                "        \"totalRentFee\": 30000,\n" +
-                "        \"eventBonus\": 100000,\n" +
-                "        \"participantCount\": 500,\n" +
-                "        \"rankInfoList\": [\n" +
-                "            {\n" +
-                "                \"rankName\": \"판매왕\",\n" +
-                "                \"rankDescription\": \"가장 많은 순수익을 얻은 사람(반기 기준)\",\n" +
-                "                \"theFirstUserInfo\": {\n" +
-                "                    \"userNickname\": \"일론 머스크\",\n" +
-                "                    \"profileIconId\": 3,\n" +
-                "                    \"profileFrameId\": 1\n" +
-                "                },\n" +
-                "                \"theFirstRecord\": 10000000,\n" +
-                "                \"myRank\": 53,\n" +
-                "                \"myRecord\": 2500000\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"rankName\": \"부자\",\n" +
-                "                \"rankDescription\": \"현재 보유 금액\",\n" +
-                "                \"theFirstUserInfo\": {\n" +
-                "                    \"userNickname\": \"젠슨 황\",\n" +
-                "                    \"profileIconId\": 2,\n" +
-                "                    \"profileFrameId\": 2\n" +
-                "                },\n" +
-                "                \"theFirstRecord\": 210000000,\n" +
-                "                \"myRank\": 112,\n" +
-                "                \"myRecord\": 10000000\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"rankName\": \"큰 손\",\n" +
-                "                \"rankDescription\": \"한 번에 돈을 제일 많이 쓴 사람(반기 기준)\",\n" +
-                "                \"theFirstUserInfo\": {\n" +
-                "                    \"userNickname\": \"김flex\",\n" +
-                "                    \"profileIconId\": 1,\n" +
-                "                    \"profileFrameId\": 1\n" +
-                "                },\n" +
-                "                \"theFirstRecord\": 1000000,\n" +
-                "                \"myRank\": 12,\n" +
-                "                \"myRecord\": 500000\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"rankName\": \"벼락부자\",\n" +
-                "                \"rankDescription\": \"한 번에 제일 많은 돈을 번 사람(반기 기준)\",\n" +
-                "                \"theFirstUserInfo\": {\n" +
-                "                    \"userNickname\": \"단타 장인\",\n" +
-                "                    \"profileIconId\": 3,\n" +
-                "                    \"profileFrameId\": 3\n" +
-                "                },\n" +
-                "                \"theFirstRecord\": 5000000,\n" +
-                "                \"myRank\": 138,\n" +
-                "                \"myRecord\": 500000\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"rankName\": \"떡상\",\n" +
-                "                \"rankDescription\": \"가장 높은 수익률(반기 기준)\",\n" +
-                "                \"theFirstUserInfo\": {\n" +
-                "                    \"userNickname\": \"제노\",\n" +
-                "                    \"profileIconId\": 1,\n" +
-                "                    \"profileFrameId\": 2\n" +
-                "                },\n" +
-                "                \"theFirstRecord\": 123.5,\n" +
-                "                \"myRank\": 1,\n" +
-                "                \"myRecord\": 123.5\n" +
-                "            }\n" +
-                "        ],\n" +
-                "        \"tteoksangStatistics\": {\n" +
-                "            \"values\": [\n" +
-                "                {\n" +
-                "                    \"productId\": 3,\n" +
-                "                    \"value\": 12.5\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"productId\": 7,\n" +
-                "                    \"value\": 11.2\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"productId\": 5,\n" +
-                "                    \"value\": 9.3\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        },\n" +
-                "        \"tteokrockStatistics\": {\n" +
-                "            \"values\": [\n" +
-                "                {\n" +
-                "                    \"productId\": 3,\n" +
-                "                    \"value\": -20.3\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"productId\": 1,\n" +
-                "                    \"value\": -17.2\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"productId\": 6,\n" +
-                "                    \"value\": -11.3\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        },\n" +
-                "        \"bestSellerStatistics\": {\n" +
-                "            \"values\": [\n" +
-                "                {\n" +
-                "                    \"productId\": 3,\n" +
-                "                    \"value\": 15000000\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"productId\": 7,\n" +
-                "                    \"value\": 12000000\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"productId\": 1,\n" +
-                "                    \"value\": 9000000\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        },\n" +
-                "        \"achievementList\": [{ \"achievementId\": 2 }, { \"achievementId\": 5 }]\n" +
-                "    },\n" +
-                "    \"quarterReport\": {\n" +
-                "        \"quarterProfit\": 1000000,\n" +
-                "        \"rentFee\": 10000,\n" +
-                "        \"inProductList\": [3, 4, 5, 7, 8],\n" +
-                "        \"titleId\": 1\n" +
-                "    },\n" +
-                "    \"participantCount\": 1000,\n" +
-                "    \"rankInfoList\": [\n" +
-                "        {\n" +
-                "            \"rankName\": \"판매왕\",\n" +
-                "            \"rankDescription\": \"가장 많은 순수익을 얻은 사람(반기 기준)\",\n" +
-                "            \"theFirstUserInfo\": {\n" +
-                "                \"userNickname\": \"일론 머스크\",\n" +
-                "                \"profileIconId\": 3,\n" +
-                "                \"profileFrameId\": 1\n" +
-                "            },\n" +
-                "            \"theFirstRecord\": 12000000,\n" +
-                "            \"myRank\": 851,\n" +
-                "            \"myRecord\": 0\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"rankName\": \"부자\",\n" +
-                "            \"rankDescription\": \"현재 보유 금액\",\n" +
-                "            \"theFirstUserInfo\": {\n" +
-                "                \"userNickname\": \"젠슨 황\",\n" +
-                "                \"profileIconId\": 2,\n" +
-                "                \"profileFrameId\": 2\n" +
-                "            },\n" +
-                "            \"theFirstRecord\": 230000000,\n" +
-                "            \"myRank\": 189,\n" +
-                "            \"myRecord\": 10000000\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"rankName\": \"큰 손\",\n" +
-                "            \"rankDescription\": \"한 번에 돈을 제일 많이 쓴 사람(반기 기준)\",\n" +
-                "            \"theFirstUserInfo\": {\n" +
-                "                \"userNickname\": \"김flex\",\n" +
-                "                \"profileIconId\": 1,\n" +
-                "                \"profileFrameId\": 1\n" +
-                "            },\n" +
-                "            \"theFirstRecord\": 3000000,\n" +
-                "            \"myRank\": 851,\n" +
-                "            \"myRecord\": 0\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"rankName\": \"벼락부자\",\n" +
-                "            \"rankDescription\": \"한 번에 제일 많은 돈을 번 사람(반기 기준)\",\n" +
-                "            \"theFirstUserInfo\": {\n" +
-                "                \"userNickname\": \"단타 장인\",\n" +
-                "                \"profileIconId\": 3,\n" +
-                "                \"profileFrameId\": 3\n" +
-                "            },\n" +
-                "            \"theFirstRecord\": 7000000,\n" +
-                "            \"myRank\": 851,\n" +
-                "            \"myRecord\": 0\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"rankName\": \"떡상\",\n" +
-                "            \"rankDescription\": \"가장 높은 수익률(반기 기준)\",\n" +
-                "            \"theFirstUserInfo\": {\n" +
-                "                \"userNickname\": \"제노\",\n" +
-                "                \"profileIconId\": 1,\n" +
-                "                \"profileFrameId\": 2\n" +
-                "            },\n" +
-                "            \"theFirstRecord\": 131.5,\n" +
-                "            \"myRank\": 500,\n" +
-                "            \"myRecord\": 0\n" +
-                "        }\n" +
-                "    ],\n" +
-                "    \"tteoksangStatistics\": {\n" +
-                "        \"values\": [\n" +
-                "            {\n" +
-                "                \"productId\": 3,\n" +
-                "                \"value\": 11.7\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"productId\": 7,\n" +
-                "                \"value\": 11.5\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"productId\": 5,\n" +
-                "                \"value\": 10.2\n" +
-                "            }\n" +
-                "        ]\n" +
-                "    },\n" +
-                "    \"tteokrockStatistics\": {\n" +
-                "        \"values\": [\n" +
-                "            {\n" +
-                "                \"productId\": 3,\n" +
-                "                \"value\": -21\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"productId\": 1,\n" +
-                "                \"value\": -18\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"productId\": 6,\n" +
-                "                \"value\": -15\n" +
-                "            }\n" +
-                "        ]\n" +
-                "    },\n" +
-                "    \"bestSellerStatistics\": {\n" +
-                "        \"values\": [\n" +
-                "            {\n" +
-                "                \"productId\": 3,\n" +
-                "                \"value\": 23000000\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"productId\": 7,\n" +
-                "                \"value\": 11000000\n" +
-                "            },\n" +
-                "            {\n" +
-                "                \"productId\": 1,\n" +
-                "                \"value\": 5000000\n" +
-                "            }\n" +
-                "        ]\n" +
-                "    }\n" +
-                "}\n";
+        String ResponseData = TestExample.offlineData;
         ObjectMapper mapper = new ObjectMapper();
         boolean isSuccess = false;
-        Quarter message = null;
+        OfflineReport message = null;
         try {
-            message = mapper.readValue(logData, Quarter.class);
+            message = mapper.readValue(ResponseData, OfflineReport.class);
             isSuccess = true;
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        GameMessageRes quarterResult = GameMessageRes.builder()
+        GameMessageRes offlineResult = GameMessageRes.builder()
                 .type(MessageType.OFFLINE_REPORT)
                 .isSuccess(isSuccess)
                 .body(message)
                 .build();
 
-        simpMessagingTemplate.convertAndSend("/topic/private/" + webSocketId, quarterResult);
+        simpMessagingTemplate.convertAndSend("/topic/private/" + webSocketId, offlineResult);
     }
 
 
