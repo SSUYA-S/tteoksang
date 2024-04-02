c17d7376c2dd8cfd7d57cd37caf4404be17ad972
김강민 <mmm3657@naver.com>
2024-04-03 00:40:18 +0900

    refactor: 파산 기능 수정 및 계절 결산 수정

--- backend/src/main/java/com/welcome/tteoksang/resource/repository/ProductRepository.java
+++ backend/src/main/java/com/welcome/tteoksang/resource/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.welcome.tteoksang.resource.repository;
 import com.welcome.tteoksang.resource.dto.Product;
 import com.welcome.tteoksang.resource.type.ProductType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface ProductRepository extends JpaRepository<Product,Integer> {
 
     List<Product> findAllByProductTypeOrProductType(ProductType season, ProductType all);
 
+    @Query("SELECT p.productId FROM Product p WHERE p.productType = ?1")
+    List<Integer> findProductIdsByProductType(ProductType productType);
 }
