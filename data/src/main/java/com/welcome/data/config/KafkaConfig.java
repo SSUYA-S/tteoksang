5c66d4a575885115106061068d2ca1d2d72ed33b
김연수 <treamor@naver.com>
2024-04-04 05:47:25 +0900

    build: 환경변수 변경

--- data/src/main/java/com/welcome/data/config/KafkaConfig.java
+++ data/src/main/java/com/welcome/data/config/KafkaConfig.java
@@ -26,17 +26,17 @@ import org.springframework.kafka.core.ProducerFactory;
 public class KafkaConfig {
 
     @Value("${DATA_KAFKA_BOOTSTRAP_SERVERS}")
-    private String KAFKA_BOOTSTRAP_SERVERS;
+    private String DATA_KAFKA_BOOTSTRAP_SERVERS;
 
     @Value("${DATA_KAFKA_CONSUMER_GROUP_ID}")
-    private String KAFKA_CONSUMER_GROUP_ID;
+    private String DATA_KAFKA_CONSUMER_GROUP_ID;
 
     // 카프카 producer 설정
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, DATA_KAFKA_BOOTSTRAP_SERVERS);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(props);
@@ -50,8 +50,8 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP_SERVERS);
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, KAFKA_CONSUMER_GROUP_ID);
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, DATA_KAFKA_BOOTSTRAP_SERVERS);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, DATA_KAFKA_CONSUMER_GROUP_ID);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
