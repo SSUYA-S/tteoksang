8a4f1f610d992d7553f09c7540754a747be592e8
김연수 <treamor@naver.com>
2024-04-08 23:14:30 +0900

    fix: change args
    Former-commit-id: ad5059175385cbd35b99da194844594d631f5f9c

--- hadoop/src/main/java/com/welcome/Main.java
+++ hadoop/src/main/java/com/welcome/Main.java
@@ -322,8 +322,8 @@ public class Main {
 			// set number of reduces
 			job.setNumReduceTasks(16);
 
-			Path inputPath = new Path(otherArgs[0]);
-			Path outputPath = new Path(otherArgs[1]);
+			Path inputPath = new Path(args[0]);
+			Path outputPath = new Path(args[1]);
 
 			fs.delete(outputPath, true);
 
