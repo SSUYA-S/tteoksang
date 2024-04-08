b8319c2c73e5473b60970eb6af20d30ff70173cd
김연수 <treamor@naver.com>
2024-04-08 22:08:27 +0900

    fix: remove Driver
    Former-commit-id: 15c8666c74c3751a380dc02934558bedff6804f0

--- hadoop/src/main/java/com/welcome/Driver.java
+++ hadoop/src/main/java/com/welcome/Driver.java
@@ -3,17 +3,17 @@ package com.welcome;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class Driver {
-    public static void main(String[] args) {
-        int exitCode = -1;
-        ProgramDriver pgd = new ProgramDriver();
-        try {
-            pgd.addClass("statistics", Main.class, "A map/reduce program that performs calculate statistics");
-            exitCode = pgd.run(args);
-        }
-        catch(Throwable e) {
-            e.printStackTrace();
-        }
-
-        System.exit(exitCode);
-    }
+//    public static void main(String[] args) {
+//        int exitCode = -1;
+//        ProgramDriver pgd = new ProgramDriver();
+//        try {
+//            pgd.addClass("main", Main.class, "A map/reduce program that performs calculate statistics");
+//            exitCode = pgd.run(args);
+//        }
+//        catch(Throwable e) {
+//            e.printStackTrace();
+//        }
+//
+//        System.exit(exitCode);
+//    }
 }
